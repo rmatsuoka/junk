40eef4fe4572df7b7457227388c2e746668fd7ba
Ryuichi Matsuoka <r.matsuoka@protonmail.com>
2022-09-04 18:04:32 +0900

    dump9p: new

---
+++ dump9p/go.mod
@@ -0,0 +1,5 @@
+module github.com/rmatsuoka/junk/dump9p
+
+go 1.17
+
+require 9fans.net/go v0.0.4
---
+++ dump9p/main.go
@@ -0,0 +1,68 @@
+package main
+
+// dump9p -- convert 9P2000 data into text.
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"9fans.net/go/plan9"
+)
+
+var (
+	stdout   io.Writer
+	exitCode = 0
+	uflag    = flag.Bool("u", false, "unbuffer")
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("dump9p: ")
+	flag.Parse()
+
+	if *uflag {
+		stdout = os.Stdout
+	} else {
+		stdout = bufio.NewWriter(os.Stdout)
+	}
+
+	if flag.NArg() == 0 {
+		look9p(os.Stdin)
+	} else {
+		for _, fname := range flag.Args() {
+			f, err := os.Open(fname)
+			if err != nil {
+				log.Print(err)
+				exitCode = 1
+				continue
+			}
+			look9p(f)
+			f.Close()
+		}
+	}
+
+	if w, ok := stdout.(*bufio.Writer); ok {
+		w.Flush()
+	}
+}
+
+func look9p(r io.Reader) {
+	if !*uflag {
+		r = bufio.NewReader(r)
+	}
+	for {
+		f, err := plan9.ReadFcall(r)
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			log.Print(err)
+			return
+		}
+		fmt.Fprintln(stdout, f)
+	}
+}
