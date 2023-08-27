391cbde879ba5b32455c511fdccd97e660bd1724
rmatsuoka <r.matsuoka@protonmail.com>
2023-08-27 12:20:13 +0900

    ls.pl change a indent from a tab to 4 spaces

--- ls.pl
+++ ls.pl
@@ -5,56 +5,56 @@ use experimental 'defer';
 use Data::Dumper;
 
 sub statdir($dir) {
-	use File::stat;
-	use File::Spec::Functions;
+    use File::stat;
+    use File::Spec::Functions;
 
-	my $prefix = $dir // "";
-	$dir = $dir // ".";
-	opendir my $dh, $dir or return;
-	defer { closedir $dh; }
+    my $prefix = $dir // "";
+    $dir = $dir // ".";
+    opendir my $dh, $dir or return;
+    defer { closedir $dh; }
 
-	my @stats;
-	foreach (readdir $dh) {
-		next if /^\.\.?$/;
-		my $name = $prefix ? catfile($prefix, $_) : $_;
-		my $stat = stat $name;
-		push @stats, { stat => $stat, name => $name };
-	}
-	return sort { $a->{name} cmp $b->{name} } @stats;
+    my @stats;
+    foreach (readdir $dh) {
+        next if /^\.\.?$/;
+        my $name = $prefix ? catfile($prefix, $_) : $_;
+        my $stat = stat $name;
+        push @stats, { stat => $stat, name => $name };
+    }
+    return sort { $a->{name} cmp $b->{name} } @stats;
 }
 
 sub format_mode($perm) {
-	my $rwx = 'rwxrwxrwx';
-	my $l = length $rwx;
-	for (0 .. $l - 1) {
-		if (($perm & (1 << $_)) == 0) {
-			substr $rwx, $l - $_ - 1, 1, '-';
-		}
-	}
-	return $rwx
+    my $rwx = 'rwxrwxrwx';
+    my $l = length $rwx;
+    for (0 .. $l - 1) {
+        if (($perm & (1 << $_)) == 0) {
+            substr $rwx, $l - $_ - 1, 1, '-';
+        }
+    }
+    return $rwx
 }
 
 sub ls($dir) {
-	use Time::Piece;
+    use Time::Piece;
 
-	foreach (statdir $dir) {
-		my $stat = $_->{stat};
-		my $rwx = format_mode $stat->mode;
-		my $t = localtime $stat->mtime;
-		printf(
-			"%s %2d %s %s %10d %s %s\n",
-			$rwx,
-			$stat->nlink,
-			scalar getgrgid $stat->gid,
-			scalar getpwuid $stat->uid,
-			$stat->size,
-			$t->strftime("%b %d %Y %H:%M"),
-			$_->{name}
-		);
-	}
+    foreach (statdir $dir) {
+        my $stat = $_->{stat};
+        my $rwx = format_mode $stat->mode;
+        my $t = localtime $stat->mtime;
+        printf(
+            "%s %2d %s %s %10d %s %s\n",
+            $rwx,
+            $stat->nlink,
+            scalar getgrgid $stat->gid,
+            scalar getpwuid $stat->uid,
+            $stat->size,
+            $t->strftime("%b %d %Y %H:%M"),
+            $_->{name}
+        );
+    }
 }
 
 unshift @ARGV, undef unless @ARGV;
 foreach (@ARGV) {
-	ls($_);
+    ls($_);
 }
